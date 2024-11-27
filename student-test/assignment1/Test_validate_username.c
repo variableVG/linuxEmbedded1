daf5a71b0183863497df9274a2fb5d8d86f7552d
variableVG <vio.garciaespin@gmail.com>
2024-11-27 19:48:22 +0100

    Assignment 1

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -4,6 +4,7 @@
 #include "../../examples/autotest-validate/autotest-validate.h"
 #include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
 
+
 /**
 * This function should:
 *   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
@@ -18,5 +19,8 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char* username1 = my_username();
+    char* username_malloc = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(username1, username_malloc, "User strings are not the same");
+    TEST_ASSERT_TRUE_MESSAGE(true,"AESD students, please fix me!");
 }
