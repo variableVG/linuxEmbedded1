d490bfde95e575985334e47c7b9f2b213c77f17d
variableVG <vio.garciaespin@gmail.com>
2024-12-03 02:00:00 +0100

    Assignment 2

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char *argv[]) {
     // Open syslog with LOG_USER facility
-    openlog("writer", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_USER);
+    openlog(NULL, 0, LOG_USER);
 
     // Check if the correct number of arguments is provided
     if (argc != 3) {
@@ -16,29 +16,32 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    const char *writefile = argv[1];
+    const char *filename = argv[1];
     const char *writestr = argv[2];
 
     // Open the file for writing
-    FILE *file = fopen(writefile, "w");
-    if (!file) {
-        syslog(LOG_ERR, "Error: Could not open file %s for writing: %s", writefile, strerror(errno));
-        fprintf(stderr, "Error: Could not open file %s for writing: %s\n", writefile, strerror(errno));
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        syslog(LOG_ERR, "Error: Could not open file %s for writing: %s", filename, strerror(errno));
+        fprintf(stderr, "Error: Could not open file %s for writing: %s\n", filename, strerror(errno));
         closelog();
         return 1;
     }
 
     // Write the string to the file
     if (fprintf(file, "%s", writestr) < 0) {
-        syslog(LOG_ERR, "Error: Could not write to file %s: %s", writefile, strerror(errno));
-        fprintf(stderr, "Error: Could not write to file %s: %s\n", writefile, strerror(errno));
+        syslog(LOG_ERR, "Value of errno attempting to open file %s: %d\n", filename, errno);
+        fprintf(stderr, "Value of errno attempting to open file %s: %d\n", filename, errno);
+        perror("perror returned");
+        syslog(LOG_ERR, "Error opening file %s: %s\n", filename, strerror(errno));
+        fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
         fclose(file);
         closelog();
         return 1;
     }
 
     // Log the successful write operation
-    syslog(LOG_DEBUG, "Writing '%s' to '%s'", writestr, writefile);
+    syslog(LOG_DEBUG, "Writing '%s' to '%s'", writestr, filename);
     fclose(file);
 
     // Close syslog
